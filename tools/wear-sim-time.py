0224da52a6d9ddcd71257d6408cc86272b11d80a
David Rein <David.Rein.ext@unit4.com>
2022-12-11 06:56:07 -0500

    Small adjustments to the script after using it for a bit

--- tools/wear-sim-time.py
+++ tools/wear-sim-time.py
@@ -5,6 +5,7 @@ from __future__ import print_function
 
 import subprocess
 import argparse
+import time
 
 
 def adb(adb_args, command):
@@ -13,16 +14,17 @@ def adb(adb_args, command):
   subprocess.call(cmd, shell=True)
 
 def main():
-
+  adb("", "shell input keyevent KEYCODE_WAKEUP")
+  time.sleep(1)
   i = 0
   while i < 60*24:
     hour = int(i / 60)
     minute = i % 60
     adb("", "shell /system/xbin/su 0 date {}:{}:00".format( hour, minute))
     adb("", "shell su 0 am broadcast -a android.intent.action.TIME_SET")
-    if minute % 60 == 0:
-      adb("", "shell input keyboard keyevent 13")
-    i = i + 2
+    if i % 240 == 0:
+      adb("", "shell input keyevent 13")
+    i = i + 4
 
 if __name__ == "__main__":
   main()
